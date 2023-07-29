444fc608bda69032f924170b57e58e330daa2db5
zion segovia <zionsegovia@gmail.com>
2023-07-29 16:42:10 -0500

    Added Start of menu funcionality

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -16,8 +16,22 @@ class MenuScene extends BaseScene{
 
     create(){
     super.create();
-    this.createMenu(this.menu);
+    this.createMenu(this.menu,(menuItem) =>  this.setupMenuEvents(menuItem));
         }
+
+     setupMenuEvents(menuItem) {
+        const textGO = menuItem.textGO;
+        textGO.setInteractive();
+
+        textGO.on('pointerover', () => {
+            textGO.setStyle({fill: '#FF0'});
+
+        })
+
+         textGO.on('pointerout', () => {
+             textGO.setStyle({fill: '#594c4c'});
+         })
+     }
 
 
 }
